a3cbfd8f161f363a4516f79061ff29cf35144580
Hexuan Li <lihexuan1@gmail.com>
2022-05-28 19:24:18 -0700

    add PortfolioContainer

--- src/App.js
+++ src/App.js
@@ -1,40 +1,6 @@
-import styles from './App.css';
-import React, { useState } from 'react';
+import React from "react";
+import PortfolioContainer from "./PortfolioContainer";
 
-import Terminal from'./Terminals/Terminal'
-import About from './Components/About/About';
-import ContactMe from './Components/ContactMe/ContactMe';
-import Experience from './Components/Experience/Experience';
-import Work from './Components/Work/Work';
+const App = () => <PortfolioContainer />;
 
-export default function PortfolioContainer() {
-    const [currentPage, setCurrentPage] = useState('Terminal');
-  
-    // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-    const renderPage = () => {
-      if (currentPage === 'Terminal') {
-        return <Terminal />;
-      }
-      if (currentPage === 'About') {
-        return <About />;
-      }
-      if (currentPage === 'Work') {
-        return <Work />;
-      }
-      if (currentPage === 'Experience') {
-        return <Experience />;
-      }
-      return <ContactMe />;
-    };
-  
-    const handlePageChange = (page) => setCurrentPage(page);
-  
-    return (
-      <div>
-        {/* We are passing the currentPage from state and the function to update it */}
-        <Terminal currentPage={currentPage} handlePageChange={handlePageChange} />
-        {/* Here we are calling the renderPage method which will return a component  */}
-        {renderPage()}
-      </div>
-    );
-  }
+export default App;
---
+++ src/PortfolioContainer.js
@@ -0,0 +1,193 @@
+//This is the portfolio container
+//Render the page and Navbar
+import React, { PureComponent } from 'react';
+import classNames from 'classnames/bind';
+import styles from './App.css';
+
+import Terminal from'./Terminals/Terminal'
+import About from './Components/About/About';
+import ContactMe from './Components/ContactMe/ContactMe';
+import Experience from './Components/Experience/Experience';
+import Work from './Components/Work/Work';
+
+class PortfolioContainer extends PureComponent {
+  state = {
+    tabs:[
+      {
+        name:"Terminal"
+        ,displayed:true,
+        id:0
+      }
+    ],
+    repos:[
+      {
+        name:"",
+        link:"",
+        description:"",
+        languages:"",
+        size:0
+      }
+    ]
+  };
+
+  addTab = (tabName) =>{
+    let tempTabs = [...this.state.tabs];
+
+    if(tabName !=="Terminal" && tempTabs.findIndex((tab)=>tab.name === tabName) !== -1){
+      let id = tempTabs[tempTabs.findIndex((tab)=>tab.name === tabName)].id;
+      this.selectTab(id);
+      return false;
+    }
+
+    tempTabs.map((tab)=>tab.displayed = false);
+    switch(tabName){   
+      case "Terminal":{
+        tempTabs.push({name:"Terminal",displayed:true});
+        break;
+      }
+
+      case "About":{
+        tempTabs.push({name:"About",displayed:true});
+        break;
+      }
+
+      case "Experience":{
+        tempTabs.push({name:"Experience",displayed:true});
+        break;
+      }
+
+      case "Work":{
+        tempTabs.push({name:"Work",displayed:true});
+        break;
+      }
+
+      case "Contact":{
+        tempTabs.push({name:"Contact",displayed:true});
+        break;
+      }
+      
+      default :{
+        return false;
+      }
+    }
+    this.setState({tabs:tempTabs});
+    return true;
+  }
+
+  removeTab = (id) =>{
+    const tempTabs = [...this.state.tabs];
+    let index = tempTabs.findIndex((tab)=>tab.displayed === true);
+    tempTabs[index].displayed = false;
+    tempTabs[0].displayed = true;
+
+    if(!Number.isInteger(parseFloat(id)))
+      index = tempTabs.findIndex((tab)=>tab.name === id);
+    else
+      index = tempTabs.findIndex((tab)=>tab.id === id);
+      
+    if(index <= 0)
+      return false;
+    
+    else
+      tempTabs.splice(index, 1);
+      
+    this.setState({tabs:tempTabs})
+    
+    return true;
+  }
+
+  selectTab = (id) =>{
+    let tempArr = [...this.state.tabs].reverse();
+    let findIndex = () =>{
+      for(let i = tempArr.length - 1; i >= 0 ;i--){
+        if(tempArr[i].id === id)
+          return i;
+      }
+      return -1;
+    }
+    let index = findIndex();
+
+
+    for(let i = 0; i < tempArr.length; i++){
+      if(i === index){
+        tempArr[i].displayed = true;
+      }
+      else
+        tempArr[i].displayed = false;
+    }
+    tempArr.reverse();
+    this.setState({tabs:tempArr});
+  }
+
+  DisplayCurrentWindow = () =>{
+    let index = this.state.tabs.findIndex((tab) => tab.displayed);
+    let result = null;
+    if(this.state.tabs[index].name === 'About')
+      result = <About></About>
+    else if(this.state.tabs[index].name === 'Experience')
+      result = <Experience></Experience>
+    else if(this.state.tabs[index].name === 'Work')
+      result = <Work repos={this.state.repos}></Work>
+    else if(this.state.tabs[index].name === 'Contact')
+      result = <ContactMe></ContactMe>
+
+    return result;
+  }
+
+  TerminalTabs(){
+    let id = 0;
+    let cx = classNames.bind(styles);
+    return (
+      <ol id="tabs">
+        {
+        this.state.tabs.map((tab)=>{
+          let classes = cx({indTab:tab.id!==0},{terminal:tab.id===0},{selectedTab:tab.displayed});
+          tab.id = id;
+          let result = (<li key={id} onClick={()=>this.selectTab(tab.id)} className={classes}>{tab.name}<b onClick={(e)=> {e.stopPropagation();this.removeTab(tab.id);}} className="closeX">X</b></li>);
+          id++;
+          return result;  
+        })}
+        <li id="addTab">+</li>
+      </ol>
+    );
+  }
+
+  RightBar(){
+    return <div className="emailLine">
+            <div className="email"><span onClick={()=> this.addTab("Contact")}>siennameow</span></div>
+           </div>
+  }
+
+  navigationButtons(){
+    return  <ol id="navBar">
+              <li onClick={()=> this.addTab("About")}>About</li>
+              <li onClick={()=> this.addTab("Experience")}>Experience</li>
+              <li onClick={()=> this.addTab("Work")}>Work</li>
+              <li onClick={()=> this.addTab("Contact")}>Contact Me</li>
+            </ol>
+  }
+
+  render () {
+    return (      
+    <div id="app">
+      {this.navigationButtons()}
+
+      {this.RightBar()}
+
+      {this.TerminalTabs()}
+
+      <Terminal 
+        addTab = {this.addTab}
+        removeTab = {this.removeTab}
+        display = {this.state.tabs[0].displayed ? "" : "hideTerminal"}
+        inView = {this.state.tabs[0].displayed}>   
+      </Terminal>
+      {this.DisplayCurrentWindow()}
+      <a href="https://github.com/siennameow/portfolio-4.0" target="_blank" id="footer" rel="noreferrer">Created and Designed by Sienna Li</a>
+    </div>
+    );
+  }
+}
+
+
+export default PortfolioContainer;
