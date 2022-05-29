6c776aeda9cb143d900a8fcf8388cd42f519e74c
Hexuan Li <lihexuan1@gmail.com>
2022-05-29 11:05:26 -0700

    add projectcard to projectpage

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import './Work.css';
-import '../../Terminals/Terminal.css'
-import { Container} from "react-bootstrap";
+import React from "react";
+import "./Work.css";
+import "../../Terminals/Terminal.css";
+import ProjectCard from "./ProjectCard";
+import { Container, Row, Col } from "react-bootstrap";
 
-const Work = () =>{
-    // const [showAllRepos,toggleShowAllRepos] = useToggle(false);
-    return(
-<Container fluid className="project-section">
-    <div className = "main">
+const Work = () => {
+  // const [showAllRepos,toggleShowAllRepos] = useToggle(false);
+  return (
+    <Container fluid className="project-section">
+      <div className="main">
         <div className="projects">
-            <div className="workHeader">
-                <h3>Projects I've Built</h3>
-            </div>
-            {/* <ProjectCard > */}
+          <div className="workHeader">
+            <h3>Projects I've Built</h3>
+          </div>
+          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            <Col md={4} className="project-card">
+              <ProjectCard 
+               //   imgPath={}
+               title="title1"
+               description="description1"
+               linkApp="Live link 1"
+               linkGithub="Github link 1"
+              />
+            </Col>
+          </Row>
         </div>
-    </div>
-</Container>
-    );
-}
+      </div>
+    </Container>
+  );
+};
 
 export default Work;
