daba255c18dfa774c0eb3dd21c7b341c9080be09
Hexuan Li <lihexuan1@gmail.com>
2022-05-29 09:36:37 -0700

    add work components

---
+++ src/Components/Work/Work.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Work.css';
+import '../../Terminals/Terminal.css'
+
+const Work = () =>{
+    // const [showAllRepos,toggleShowAllRepos] = useToggle(false);
+    return(
+    <div className = "main">
+        <div className="projects">
+            <div className="workHeader">
+                <h3>Projects I've Built</h3>
+            </div>
+            {/* <ProjectCard > */}
+        </div>
+    </div>
+    );
+}
+
+export default Work;
