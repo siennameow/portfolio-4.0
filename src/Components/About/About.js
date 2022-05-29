308e8c91d957c4843f9303011645f41f6efd4a20
Hexuan Li <lihexuan1@gmail.com>
2022-05-28 17:42:28 -0700

    add experience components

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -14,12 +14,10 @@ return(
                     <h1>About Me</h1>
                 </div>
                 <p>Hello there! I'm a web developer and designer based in San Francisco Bay Area.<br/>⠀</p>
+                <p>Having spent a large chunk of my life in arts and music, I've finally found my passion at the web design and technology. I'm an exceptionally creative designer and a coding elf.<br/>⠀</p>
                 <p>Currently I'm a student at <a className="school"href="https://bootcamp.berkeley.edu/coding/" target="_blank" rel="noreferrer">UC Berkeley Bootcamp</a> and spend most of my time designing websites. 
                 When I'm not busy, you can find me obsessing over food, cats and traveling. 
-                <br/>⠀
-                </p>
-                <p>Here are a few technologies I've worked with recently:<br/>⠀</p>
-                {getTechnologies()}
+                <br/>⠀</p>
             </div>
             <div className="avatar"></div>
         </div>
@@ -27,15 +25,5 @@ return(
     );
 }
 
-const getTechnologies = () =>{
-    return <ol className="technologies">
-                <li>HTML & CSS</li>
-                <li>JavaScript</li>
-                <li>Express.js</li>
-                <li>Node.js</li>
-                <li>React</li>
-                <li>MongoDB</li>
-            </ol>
-}
 
 export default About;
---
+++ src/Components/Experience/Experience.js
@@ -0,0 +1,42 @@
+//WHEN I am presented with the Resume section
+//THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
+
+import React from 'react';
+import './Experience.css';
+import '../../Terminals/Terminal.css'
+
+const Experience = () =>{
+    return(
+    <div className = "experience main">
+        <div>
+            <div className="container">
+                <span className="title">
+                    <h1>Download my <a className="resume" href="./Resources/util/resume.pdf" style={{color: "pink", fontSize: "40px"}} target="_blank" rel="noreferrer" >Resume</a></h1>
+                </span>
+                <br></br>
+                <p>Here are a few technologies I've worked with recently:<br/>⠀</p>
+               <ol className="technologies">
+                <li>HTML & CSS</li>
+                <li>Bootstrap</li>
+                <li>JavaScript</li>
+                <li>Express.js</li>
+                <li>Node.js</li>
+                <li>React</li>
+                <li>MongoDB</li>
+                <li>MySQL</li>
+                <li>Handlebars</li>
+                <li>Sequelize</li>
+                <li>Insomnia</li>
+                <li>Heroku</li>
+                <li>Frontend Development</li>
+                <li>Server Side API & Third Party API</li>
+                <li>Backend Development</li>
+                <li>Full Stack Development</li>
+            </ol>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Experience;
